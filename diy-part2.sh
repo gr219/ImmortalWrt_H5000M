c8ef2cac64d8ef057d3b76a92d0908e4220093d3
Tuyến Trần <mr.trungtuyen@gmail.com>
2026-05-05 00:03:22 +0700

    Update copyright year and clean up comments

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,140 +1,20 @@
 #!/bin/bash
 #
-# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 # https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
+# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
 
-# 更改默认IP
+# Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
-#添加软件包
-#OpenClash
-#rm -rf feeds/luci/applications/luci-app-openclash
-#git clone -b master --single-branch --filter=blob:none https://github.com/vernesong/OpenClash.git feeds/luci/applications/luci-app-openclash
-#AdguardHome
-#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-#Mihomo
-#git clone -b main --single-branch --filter=blob:none https://github.com/nikkinikki-org/OpenWrt-nikki
-#mv OpenWrt-nikki/luci-app-nikki package/
-#mv OpenWrt-nikki/nikki package/
-#添加qosmate
-#git clone https://github.com/hudra0/qosmate.git package/qosmate
-#git clone https://github.com/LemonCrab666/luci-app-qosmate.git package/luci-app-qosmate
+# Modify default theme
+#sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 
-#修改nginx默认http
-mkdir -p files/etc/config
-
-cat <<EOF > files/etc/config/nginx
-config main global
-	option uci_enable 'true'
-
-config server '_lan'
-	list listen '8888 ssl default_server'
-	list listen '[::]:8888 ssl default_server'
-	option server_name '_lan'
-	list include 'restrict_locally'
-	list include 'conf.d/*.locations'
-	option uci_manage_ssl 'self-signed'
-	option ssl_certificate '/etc/nginx/conf.d/_lan.crt'
-	option ssl_certificate_key '/etc/nginx/conf.d/_lan.key'
-	option ssl_session_cache 'shared:SSL:32k'
-	option ssl_session_timeout '64m'
-	option access_log 'off; # logd openwrt'
-
-config server '_redirect2ssl'
-	list listen '8887'
-	list listen '[::]:8887'
-	option server_name '_redirect2ssl'
-	option return '302 https://$host$request_uri'
-EOF
-
-chmod 0600 files/etc/config/nginx
-
-#修改sysguarde备份列表
-mkdir -p files/etc
-
-cat <<EOF > files/etc/sysupgrade.conf
-## This file contains files and directories that should
-## be preserved during an upgrade.
-
-# /etc/example.conf
-# /etc/openvpn/
-
-EOF
-
-chmod 0644 files/etc/sysupgrade.conf
-
-#将clash内核、TUN内核、Meta内核编译进目录
-#mkdir -p files/etc/openclash/core
-#Dev内核
-#curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-amd64.tar.gz | tar -xz -C /tmp
-#mv /tmp/clash files/etc/openclash/core/clash
-#chmod 0755 files/etc/openclash/core/clash
-#tun内核
-#curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-amd64-2023.08.17-13-gdcc8d87.gz | gunzip -c > /tmp/clash_tun
-#mv /tmp/clash_tun files/etc/openclash/core/clash_tun
-#chmod 0755 files/etc/openclash/core/clash_tun
-#meta内核
-#curl -L https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-amd64.tar.gz | tar -xz -C /tmp
-#mv /tmp/clash files/etc/openclash/core/clash_meta
-#chmod 0755 files/etc/openclash/core/clash_meta
-
-#将AdGuardHome核心文件编译进目录
-#curl -s https://api.github.com/repos/AdguardTeam/AdGuardHome/releases/latest \
-#| grep "browser_download_url.*AdGuardHome_linux_amd64.tar.gz" \
-#| cut -d : -f 2,3 \
-#| tr -d \" \
-#| xargs curl -L -o /tmp/AdGuardHome_linux_amd64.tar.gz && \
-#tar -xzvf /tmp/AdGuardHome_linux_amd64.tar.gz -C /tmp/ --strip-components=1 && \
-#mkdir -p files/usr/bin/AdGuardHome && \
-#mv /tmp/AdGuardHome/AdGuardHome files/usr/bin/AdGuardHome/
-#chmod 0755 files/usr/bin/AdGuardHome/AdGuardHome
-
-# 修改固件MD5值
-# 生成VerMagic文件
-echo "e7e50fbc0aafa7443418a79928da2602" > vermagic
-# 检查VerMagic文件是否生成成功
-if [ ! -f "vermagic" ]; then
-    echo "VerMagic文件生成失败！"
-    exit 1
-fi
-
-# 修改include/kernel-defaults.mk
-# 设置变量
-pattern="grep '=[ym]' \$(LINUX_DIR)/.config.set | LC_ALL=C sort | \$(MKHASH) md5 > \$(LINUX_DIR)/.vermagic"
-replacement="cp \$(TOPDIR)/vermagic \$(LINUX_DIR)/.vermagic"
-# 对pattern中的特殊字符进行转义处理
-escaped_pattern=$(printf '%s\n' "$pattern" | sed -e 's/[][\/$*.^|[]/\\&/g')
-# 使用sed命令替换整段语句
-sed -i "s|$escaped_pattern|$replacement|g" include/kernel-defaults.mk
-# 检查替换是否成功
-if [ $? -ne 0 ]; then
-    echo "include/kernel-defaults.mk 替换失败！"
-    exit 1
-fi
-
-# 修改package/kernel/linux/Makefile
-sed -i 's/STAMP_BUILT:=$(STAMP_BUILT)_$(shell $(SCRIPT_DIR)\/kconfig.pl $(LINUX_DIR)\/.config | $(MKHASH) md5)/STAMP_BUILT:=$(STAMP_BUILT)_$(shell cat $(LINUX_DIR)\/.vermagic)/g' package/kernel/linux/Makefile
-# 检查替换是否成功
-if [ $? -ne 0 ]; then
-    echo "package/kernel/linux/Makefile 替换失败！"
-    exit 1
-fi
-
-# 输出成功信息
-echo "替换成功！"
-
-#将nlbwmon从服务目录移动到菜单栏
-sed -i -e '/"path": "admin\/services\/nlbw\/display"/d' -e 's/services\///g' -e 's/"type": "alias"/"type": "firstchild"/' package/feeds/luci/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
-sed -i 's|admin/services/nlbw/backup|admin/nlbw/backup|g' package/feeds/luci/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
-
-#由于内核参数 net.core.rmem_max 的限制，缓冲区大小被限制为 212992 字节，永久设置 Netlink 接收缓冲区大小为 524288 字节。
-mkdir -p files/etc
-echo "# Defaults are configured in /etc/sysctl.d/* and can be customized in this file" > files/etc/sysctl.conf
-echo "net.core.rmem_max=524288" >> files/etc/sysctl.conf
+# Modify hostname
+#sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
